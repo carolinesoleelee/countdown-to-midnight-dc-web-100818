ec7912efde322d000e40bbced381a992db36d5a8
Solee Lee <carolinesoleelee@gmail.com>
2018-09-21 19:12:12 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(x)
   x = 10
   while x > -1
     puts "#{x} SECOND(S)"
   x -= 1
 if x == 0
   return "HAPPY NEW YEARS!"
-end
+    end
   end
 end
 
